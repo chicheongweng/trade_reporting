345abfce94dafa858ebadd3d2b986903d96e65bb
User <User@Users-MacBook-Pro-2.local>
2018-04-10 10:25:01 +0800

    init check-in

---
+++ trade_reporting/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for trade_reporting project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("trade_reporting"))
+
+application = get_wsgi_application()
