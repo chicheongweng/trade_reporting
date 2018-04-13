41b2720b62c15423914f837f8a913a0fb4f85a68
Chi-Cheong Weng <chicheongweng@gmail.com>
2018-04-13 10:42:06 +0800

    added OrderAdmin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from orders.models import Order
 
 # Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Order, OrderAdmin)
