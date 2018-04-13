0b7ed72776d84813c798f610380d995766bef639
Chi-Cheong Weng <chicheongweng@gmail.com>
2018-04-13 15:44:02 +0800

    added generate_fake_users command

--- orders/management/commands/generate_fake_users.py
+++ orders/management/commands/generate_fake_users.py
@@ -1,1 +1,45 @@
 #https://medium.com/@gis10kwo/how-to-create-fake-jobseeker-profiles-using-django-2-0-and-faker-dcfb09bda378
+
+from django.db import models
+from django.core.management.base import BaseCommand
+from faker import Faker
+import random
+import uuid
+from django.contrib.auth.models import User
+from datetime import datetime,timedelta
+from random import randint
+from django.contrib.auth.hashers import make_password
+
+fake_en = Faker('en_US')
+fake=Faker('en_US')
+
+end=datetime.now()
+start=end-timedelta(days=365*3)
+
+def random_username():
+    return fake_en.first_name()+fake_en.last_name()+str(randint(0,999))
+
+def random_date():
+    return start+(end-start)*random.random()
+
+class Command(BaseCommand):
+
+    help = 'generate 10K Jobseeker profiles for Addis Ababa'
+
+    def handle(self, *args, **options):
+        for i in range(100):
+            id = str(uuid.uuid4())
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            username = random_username().lower()
+
+            new_user = User.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=username+"@gmail.com",
+                password=make_password("Abcde12345")
+            )
+
+            new_user.save()
+            self.stdout.write("user.id %i" % new_user.id)
