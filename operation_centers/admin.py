4433995d1466b936adbd69d516f7ce3e91dfbdeb
Chi-Cheong Weng <chicheongweng@gmail.com>
2018-04-16 22:01:52 +0800

    added search_fields to OperationCenterAdmin and ProfitAdmin

--- operation_centers/admin.py
+++ operation_centers/admin.py
@@ -4,7 +4,8 @@ from operation_centers.models import OperationCenter
 # Register your models here.
 
 class OperationCenterAdmin(admin.ModelAdmin):
-    list_display = ['name', 'users']
+    list_display = ['name', 'users', 'net']
+    search_fields = ['user__username']
 
     def users(self, obj):
         return ", ".join([k.user.username for k in obj.profile_set.all()])
--- profits/admin.py
+++ profits/admin.py
@@ -3,5 +3,6 @@ from profits.models import Profit
 
 class ProfitAdmin(admin.ModelAdmin):
     list_display = ['user', 'symbol', 'net']
+    search_fields = ['user__username']
 
 admin.site.register(Profit, ProfitAdmin)
