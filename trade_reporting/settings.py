9bf6cf1f16aa13bd318a4a9db8f245564ba7e59a
Chi-Cheong Weng <chicheongweng@gmail.com>
2018-04-17 14:43:08 +0800

    set TZ to Asia/Hong_Kong

--- trade_reporting/settings.py
+++ trade_reporting/settings.py
@@ -107,7 +107,7 @@ ALLOWED_HOSTS = ["*"]
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Hong_Kong'
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
