50963756516881863ca15ce5b3a3b19b847a64b5
Chi-Cheong Weng <chicheongweng@gmail.com>
2018-04-18 17:08:47 +0800

    used ugettext_lazy as _ for orders, profits and operation_centers verbose_name

--- operation_centers/models.py
+++ operation_centers/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class OperationCenter(models.Model):
